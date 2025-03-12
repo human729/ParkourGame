ceb2942a37d520ea9283786e092835d80949e599
Fragile9090 <gordeevglebanufa@gmail.com>
2025-03-12 10:16:50 +0300

    animatin + base movement

--- ParkourGame/Assets/Scripts/PlayerMovement.cs
+++ ParkourGame/Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,57 +9,100 @@ public class PlayerMovement : MonoBehaviour
     [Header("Movement")]
     public float MoveSpeed;
 
-    //public Transform Orientation;
-
     float horizontalInput;
     float verticalInput;
     float turnSmoothVelocity;
-    Vector3 moveDirection;
-    [SerializeField] CharacterController controller;
-
-    Rigidbody rb;
-
-    //public ThirdPersonCam Cam;
+    Vector3 PlayerVelocity;
+    CharacterController controller;
+    public float GravityValue;
+    public float JumpHeight;
+    Vector3 moveDir;
+    public float DashTime;
+    public float DashDistance;
+    private bool isMoving;
+    Animator PlayerAnimator;
+    float AnimFloat;
+    float FallingY;
 
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        AnimFloat = 0;
         controller = GetComponent<CharacterController>();
-        rb.freezeRotation = true;
+        PlayerAnimator = transform.GetChild(0).GetComponent<Animator>();
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
+        bool GroundetPlayer = controller.isGrounded;
 
         Vector3 InputVector = new Vector3 (horizontalInput,0,verticalInput).normalized;
-
+        if (MathF.Round(AnimFloat, 4) != 0.0001f)
+        {
+            AnimFloat = Mathf.Lerp(AnimFloat, 0, 0.015f);
+        }
+        PlayerAnimator.SetFloat("move", AnimFloat);
+        
         if (InputVector.magnitude >= 0.1f)
         {
-            float targetAngle = Mathf.Atan2(InputVector.x,InputVector.z) * Mathf.Rad2Deg + Camera.main.transform.eulerAngles.y;
+            
+            float targetAngle = Mathf.Atan2(InputVector.x, InputVector.z) * Mathf.Rad2Deg + Camera.main.transform.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, 0.1f);
-            transform.rotation = Quaternion.Euler(0,angle,0);
+            transform.rotation = Quaternion.Euler(0, angle, 0);
 
-            Vector3 moveDir = Quaternion.Euler(0, angle, 0) * new Vector3(0,0,1);
+            moveDir = Quaternion.Euler(0, angle, 0) * new Vector3(0, 0, 1);
 
-            //controller.Move(moveDir.normalized* MoveSpeed* Time.deltaTime);
+            controller.Move(moveDir.normalized * MoveSpeed * Time.deltaTime);
+            AnimFloat = Mathf.Lerp(AnimFloat, 0.2880295f, 1.1f);
+            PlayerAnimator.SetFloat("move", AnimFloat);
+            print(Mathf.Lerp(AnimFloat, 0.2880295f, 0.001f));
+            
 
-            rb.velocity = moveDir.normalized * MoveSpeed * Time.deltaTime;
-
-            //moveDirection.y += -9.81f * Time.deltaTime;
-            //controller.Move(moveDirection * Time.deltaTime);
+            
         }
+
+
+
+        print(GroundetPlayer);
+        if (Input.GetKeyDown(KeyCode.Space) && GroundetPlayer)
+        {
+            PlayerVelocity.y = Mathf.Sqrt(JumpHeight * -2 * GravityValue);
+            PlayerAnimator.SetBool("hasJumped", true);
+            FallingY = PlayerVelocity.y;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+
+            StartCoroutine(Dash());
+            
+        }
+        if (!GroundetPlayer)
+        {
+            PlayerAnimator.SetBool("isFalling", true);
+            PlayerAnimator.SetBool("hasJumped", false);
+        }
+        if (GroundetPlayer)
+        {
+            PlayerAnimator.SetBool("isFalling", false);
+        }
+        PlayerVelocity.y += GravityValue * Time.deltaTime;
+        controller.Move(PlayerVelocity * Time.deltaTime);
         
-        //Vector3 moveDirection = new Vector3 (transform.forward.x, rb.velocity.y, transform.forward.z);
-        
-        
+
+
     }
 
-    private void FixedUpdate()
+    IEnumerator Dash()
     {
-        
+        float startTime = Time.time;
+        while (startTime + DashTime > Time.time)
+        {
+            controller.Move(moveDir * Time.deltaTime * DashDistance);
+            yield return null;
+        }
     }
 }
--- ParkourGame/Assets/UI/UIScripts/SoundToggle.cs
+++ ParkourGame/Assets/UI/UIScripts/SoundToggle.cs
@@ -11,7 +11,8 @@ public class SoundToggle : MonoBehaviour
     public AudioMixer Mixer;
     public Image On;
     public Image Off;
-
+    float MusicValue;
+    float SoundValue;
     void Start()
     {
         toggie.isOn = SettingsSO.SoundToggle;
@@ -22,17 +23,20 @@ public class SoundToggle : MonoBehaviour
     public void ToggleSound(bool isOn)
     {
         if (isOn)
-        {
-            Mixer.SetFloat("Music", 100);
-            Mixer.SetFloat("Sounds", 100);
+        {           
+            Mixer.SetFloat("Music", MusicValue);
+            Mixer.SetFloat("Sounds", SoundValue);
             On.enabled = true; Off.enabled = false;
             transform.GetChild(1).gameObject.GetComponent<Text>().text = "ON";
         } else if (!isOn)
         {
+            Mixer.GetFloat("Sounds", out SoundValue);
+            Mixer.GetFloat("Music", out MusicValue);
             Mixer.SetFloat("Music", -80);
             Mixer.SetFloat("Sounds", -80);
             On.enabled = false; Off.enabled = true;
             transform.GetChild(1).gameObject.GetComponent<Text>().text = "OFF";
         }
     }
+
 }
